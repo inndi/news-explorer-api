6f2b7d95449e7b2b97693490dc3a697f191c0c86
Inna <inndi.spivakova@gmail.com>
2022-07-13 19:54:14 +0300

    feat: Create a schema and model for article and user; create gitignore, eslintrc and editconfig files

---
+++ models/article.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const articleSchema = mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  source: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return validator.isURL(v);
+      },
+      message: (props) => `${props.value} is not a valid link!`
+    }
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return validator.isURL(v);
+      },
+      message: (props) => `${props.value} is not a valid link!`
+    }
+  },
+  owner: {
+    default: 'userId'////////////////////////////////////////
+  }
+});
+
+module.exports = mongoose.model('article', articleSchema);
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return validator.isEmail(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  }
+});
+
+module.exports = mongoose.model('user', userSchema);
